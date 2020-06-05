b7a4686d1706f984ef84c3ceb7d99b6a7f7bbe1d
Katlego Mkhonza <katlegoalpha@gmail.com>
2020-06-05 05:48:12 +0200

    Calculator structure and styling

---
+++ js/calc.js
@@ -0,0 +1,18 @@
+let currentTotal = 0;
+let buffer = "0";
+let prevCalcAct;
+
+const screen = document.querySelector('.screen');
+
+function ClickButton(value) {
+	console.log(value);
+}
+
+function initialize() {
+	document.querySelector('.cal-buttons')
+		.addEventListener('click', function(event) {
+			ClickButton(event.target.innerText);
+		})
+}
+
+initialize();
